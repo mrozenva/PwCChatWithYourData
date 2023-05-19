d939cecf0cbfc72f5e4908fa9fa2ddd0a79f37b8
mrozenva <mrozenva@gmail.com>
2023-05-18 22:30:26 -0400

    next config change

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   webpack(config) {
+//     config.experiments = { ...config.experiments, topLevelAwait: true };
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,6 +18,20 @@ const nextConfig = {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
 };
-
+ 
 export default nextConfig;
+ 
--- utils/pinecone-client.ts
+++ utils/pinecone-client.ts
@@ -1,23 +1,56 @@
+// import { PineconeClient } from '@pinecone-database/pinecone';
+
+// if (!process.env.PINECONE_ENVIRONMENT || !process.env.PINECONE_API_KEY) {
+//   throw new Error('Pinecone environment or api key vars missing');
+// }
+
+// async function initPinecone() {
+//   try {
+//     const pinecone = new PineconeClient();
+
+//     await pinecone.init({
+//       environment: process.env.PINECONE_ENVIRONMENT ?? '', //this is in the dashboard
+//       apiKey: process.env.PINECONE_API_KEY ?? '',
+//     });
+
+//     return pinecone;
+//   } catch (error) {
+//     console.log('error', error);
+//     throw new Error('Failed to initialize Pinecone Client');
+//   }
+// }
+
+// export const pinecone = await initPinecone();
+
+
+/**
+* This pinecone client is used in your api routes via the Frontend UI
+*/
+ 
 import { PineconeClient } from '@pinecone-database/pinecone';
-
-if (!process.env.PINECONE_ENVIRONMENT || !process.env.PINECONE_API_KEY) {
-  throw new Error('Pinecone environment or api key vars missing');
-}
-
-async function initPinecone() {
-  try {
-    const pinecone = new PineconeClient();
-
-    await pinecone.init({
-      environment: process.env.PINECONE_ENVIRONMENT ?? '', //this is in the dashboard
-      apiKey: process.env.PINECONE_API_KEY ?? '',
-    });
-
-    return pinecone;
-  } catch (error) {
-    console.log('error', error);
-    throw new Error('Failed to initialize Pinecone Client');
-  }
-}
-
-export const pinecone = await initPinecone();
+import { CreatePineconeIndexArgs } from '@/types/pinecone';
+ 
+//use this client in api routes
+export const createPineconeIndex = async ({
+  pineconeApiKey,
+  pineconeEnvironment,
+  pineconeIndexName,
+}: CreatePineconeIndexArgs) => {
+  const pinecone = new PineconeClient();
+ 
+  // console.log('pineconeApiKey', pineconeApiKey);
+  // console.log('pineconeEnvironment', pineconeEnvironment);
+  // console.log('pineconeIndexName', pineconeIndexName);
+  // console.log('pinecone', pinecone);
+ 
+  await pinecone.init({
+    apiKey: pineconeApiKey ?? '',
+    environment: pineconeEnvironment ?? '', //this is in the pinecone dashboard
+  });
+ 
+  const index = pinecone.Index(pineconeIndexName);
+ 
+  console.log('index', index);
+ 
+  return index;
+};
